f112f1de2092891e0b02cba5b99833ea45bbc58e
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2023-03-05 15:36:52 +1300

    Support dragging in the level editor

--- client/src/lil_bits/editor.rs
+++ client/src/lil_bits/editor.rs
@@ -3,11 +3,17 @@ use core::{
     messages::PlayerMessage,
 };
 
-use eframe::egui::{self, Margin, Sense};
+use eframe::egui::{self, Id, Margin, Sense};
 
 use crate::theming::Theme;
 
 use super::{character::CharacterOrient, CharacterUI, EditorBarEdge, EditorBarUI, EditorSquareUI};
+
+#[derive(Clone)]
+enum EditorDrag {
+    Enabling,
+    Disabling,
+}
 
 pub struct EditorUI<'a> {
     board: &'a mut Board,
@@ -36,13 +42,48 @@ impl<'a> EditorUI<'a> {
                 for (rownum, row) in self.board.squares.iter_mut().enumerate() {
                     ui.horizontal(|ui| {
                         for (colnum, square) in row.iter_mut().enumerate() {
-                            let coord = Coordinate::new(colnum, rownum);
-
-                            if EditorSquareUI::new()
+                            let response = EditorSquareUI::new()
                                 .enabled(square.is_some())
-                                .render(ui, theme)
-                                .clicked()
-                            {
+                                .render(ui, theme);
+
+                            if ui.rect_contains_pointer(response.rect) {
+                                // TODO: This shouldn't be mut
+                                // https://github.com/emilk/egui/issues/2741
+                                let drag_action = ui.memory_mut(|mem| {
+                                    if mem.is_anything_being_dragged() {
+                                        mem.data.get_temp::<EditorDrag>(Id::null())
+                                    } else {
+                                        None
+                                    }
+                                });
+
+                                match (drag_action, &square) {
+                                    (Some(EditorDrag::Enabling), None) => {
+                                        *square = Some(Square::Empty)
+                                    }
+                                    (Some(EditorDrag::Disabling), Some(_)) => *square = None,
+                                    _ => {}
+                                }
+                            }
+                            if response.drag_started() {
+                                ui.ctx().memory_mut(|mem| {
+                                    mem.data.insert_temp(
+                                        Id::null(),
+                                        if square.is_some() {
+                                            EditorDrag::Disabling
+                                        } else {
+                                            EditorDrag::Enabling
+                                        },
+                                    )
+                                });
+                            }
+                            // Chain these next two together so that the drag end takes precedence,
+                            // otherwise we double flip. Second branch remains to cover states without
+                            // drag support, perhaps?
+                            if response.drag_released() {
+                                ui.ctx()
+                                    .memory_mut(|mem| mem.data.remove::<EditorDrag>(Id::null()));
+                            } else if response.clicked() {
                                 if square.is_some() {
                                     *square = None;
                                 } else {
