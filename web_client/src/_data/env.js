67f3e78fdc72be158705ce1b7f25a2d8b95cff9a
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2023-08-07 15:08:17 +1200

    Add Fathom event tracking to the initial game menu

--- web_client/src/_data/env.js
+++ web_client/src/_data/env.js
@@ -2,5 +2,20 @@ module.exports = function () {
     return {
         commit: process.env.TR_COMMIT || "local",
         analytics_id: "YAAFZLJM",
+        analytics_events: {
+            CANCEL_REJOIN: "S3IDITQU",
+            ERRORED_TRUNCATE: "6HOMNAAM",
+            JOIN_BY_LINK: "T5PZKKEB",
+            JOIN_ROOM: "IBUYCIZS",
+            LOADED_TRUNCATE: "CD51HVHQ",
+            MULTI_PLAYER: "GSO5TOAP",
+            PLAY_GAME: "NTZC8LNN",
+            PROMPT_REJOIN: "QLBNPWKR",
+            REJOIN: "5PCPKXJK",
+            START_MULTI: "UO5FIBLN",
+            START_SOLO: "3G8B6E2H",
+            START_TUTORIAL: "OYNYPXGD",
+            VIEW_CREDITS: "YEJSMFAQ",
+        }
     };
 };
