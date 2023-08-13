e4cc3b2205194dc0dd84618d7a4dd5327782d573
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2023-08-14 10:25:05 +1200

    Swap prose font to Micro Six

--- truncate_client/src/app_outer.rs
+++ truncate_client/src/app_outer.rs
@@ -46,7 +46,7 @@ impl OuterApplication {
         {
             fonts.font_data.insert(
                 "pixel".into(),
-                egui::FontData::from_static(include_bytes!("../font/PixelOperator.ttf")),
+                egui::FontData::from_static(include_bytes!("../font/Micro-Six.ttf")),
             );
             fonts
                 .families
