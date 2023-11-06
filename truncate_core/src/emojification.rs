b7b4aaa11263538414ace51e1fafd3a5105e7266
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2023-11-06 22:47:57 +1300

    More abstract emojification

--- truncate_core/src/emojification.rs
+++ truncate_core/src/emojification.rs
@@ -12,13 +12,13 @@ const SQ_BLACK: &str = "⬛";
 const SQ_BLACK_IN_WHITE: &str = "🔳";
 const SQ_WHITE_IN_BLACK: &str = "🔲";
 const SQ_ERR: &str = "🆘";
-const SQ_TOWN: &str = "🏘";
-const SQ_DESTROYED: &str = "🏚";
-const SQ_STAR: &str = "🌟";
-const SQ_BOAT: &str = "⛵️";
 
 impl Board {
     pub fn emojify(&self, won: Option<usize>) -> String {
+        let water = if won == Some(0) { SQ_BLUE } else { SQ_BLACK };
+        let land = if won == Some(0) { SQ_GREEN } else { SQ_BROWN };
+        let tile = if won == Some(0) { SQ_YELLOW } else { SQ_ORANGE };
+
         let mut grid = self
             .squares
             .iter()
@@ -27,44 +27,71 @@ impl Board {
                 row.iter()
                     .rev()
                     .map(|sq| match sq {
-                        crate::board::Square::Water => SQ_BLACK,
-                        crate::board::Square::Land => SQ_GREEN,
-                        crate::board::Square::Town { defeated, .. } if *defeated => SQ_DESTROYED,
-                        crate::board::Square::Town { defeated, .. } if !*defeated => SQ_TOWN,
-                        crate::board::Square::Town { .. } => SQ_ERR,
-                        crate::board::Square::Dock(_) => SQ_BOAT,
-                        crate::board::Square::Occupied(player, _) if *player == 0 => SQ_YELLOW,
-                        crate::board::Square::Occupied(player, _) if *player == 1 => SQ_BROWN,
-                        crate::board::Square::Occupied(_, _) => SQ_ERR,
+                        crate::board::Square::Water => water,
+                        crate::board::Square::Land => land,
+                        crate::board::Square::Town { .. } => land,
+                        crate::board::Square::Dock(_) => water,
+                        crate::board::Square::Occupied(player, _) if won == Some(*player) => tile,
+                        crate::board::Square::Occupied(_, _) => land,
                     })
                     .collect::<Vec<_>>()
             })
             .collect::<Vec<_>>();
 
-        while grid.iter().all(|row| row.first() == Some(&SQ_BLACK)) {
-            grid.iter_mut().for_each(|row| {
-                row.remove(0);
-            });
+        enum D {
+            Top,
+            Bottom,
+            Left,
+            Right,
+        }
+        fn trim_grid(grid: &mut Vec<Vec<&str>>, dir: D) {
+            match dir {
+                D::Top => {
+                    grid.remove(0);
+                }
+                D::Bottom => {
+                    grid.remove(grid.len() - 1);
+                }
+                D::Left => grid.iter_mut().for_each(|row| {
+                    row.remove(0);
+                }),
+                D::Right => grid.iter_mut().for_each(|row| {
+                    row.remove(row.len() - 1);
+                }),
+            };
         }
 
-        while grid.iter().all(|row| row.last() == Some(&SQ_BLACK)) {
-            grid.iter_mut().for_each(|row| {
-                row.remove(row.len() - 1);
-            });
-        }
-
+        // Remove all non-tile rows from the top
         while grid
             .first()
-            .is_some_and(|row| row.iter().all(|s| s == &SQ_BLACK))
+            .is_some_and(|row| !row.iter().any(|s| s == &tile))
         {
-            grid.remove(0);
+            trim_grid(&mut grid, D::Top);
         }
 
+        // Remove all non-tile rows from the bottom
         while grid
             .last()
-            .is_some_and(|row| row.iter().all(|s| s == &SQ_BLACK))
+            .is_some_and(|row| !row.iter().any(|s| s == &tile))
         {
-            grid.remove(grid.len() - 1);
+            trim_grid(&mut grid, D::Bottom);
+        }
+
+        // Remove all non-tile columns from the left
+        while !grid.iter().any(|row| row.first() == Some(&tile)) {
+            trim_grid(&mut grid, D::Left);
+        }
+
+        // Remove all non-tile columns from the right
+        while !grid.iter().any(|row| row.last() == Some(&tile)) {
+            trim_grid(&mut grid, D::Right);
+        }
+
+        // Keep thinning the sides until we get to 7 emoji
+        while grid.first().is_some_and(|row| row.len() > 7) {
+            let left = grid.iter().filter(|row| row.first() == Some(&tile)).count();
+            let right = grid.iter().filter(|row| row.first() == Some(&tile)).count();
+            trim_grid(&mut grid, if left < right { D::Left } else { D::Right });
         }
 
         let joined_grid = grid
