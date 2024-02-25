a2c619e9fd29c227842d126159c2382022814f16
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2024-02-26 12:54:45 +1300

    Show daily actions in the modal whether or not you win

--- truncate_client/src/lil_bits/result_modal/daily_actions.rs
+++ truncate_client/src/lil_bits/result_modal/daily_actions.rs
@@ -71,61 +71,28 @@ impl DailyActions {
 
             ui.add_space(ui.available_height() * 0.05);
 
-            if self.won_today {
-                let text = TextHelper::heavy("PLAY AGAIN", 12.0, None, ui);
-                let try_again_button =
-                    text.centered_button(theme.button_secondary, theme.text, map_texture, ui);
-                if try_again_button.clicked() {
-                    msg = Some(ResultModalAction::TryAgain);
-                }
+            let text = TextHelper::heavy("PLAY AGAIN", 12.0, None, ui);
+            let try_again_button =
+                text.centered_button(theme.button_secondary, theme.text, map_texture, ui);
+            if try_again_button.clicked() {
+                msg = Some(ResultModalAction::TryAgain);
+            }
 
-                let row = TextHelper::heavy(
-                    "Try for a better score?".into(),
-                    10.0,
-                    Some(ui.available_width()),
-                    ui,
-                );
-                row.paint(theme.button_secondary, ui, true);
+            let row = TextHelper::heavy(
+                "Try for a better score?".into(),
+                10.0,
+                Some(ui.available_width()),
+                ui,
+            );
+            row.paint(theme.button_secondary, ui, true);
 
-                ui.add_space(ui.available_height() * 0.05);
+            ui.add_space(ui.available_height() * 0.05);
 
-                if let Some(replay_link) = &self.replay_link {
-                    let button_text = if self.replay_copied {
-                        "COPIED LINK!"
-                    } else {
-                        "SHARE REPLAY"
-                    };
-                    let text = TextHelper::heavy(button_text, 12.0, None, ui);
-                    let share_button =
-                        text.centered_button(theme.button_primary, theme.text, map_texture, ui);
-
-                    if share_button.clicked()
-                        || share_button.drag_started()
-                        || share_button.is_pointer_button_down_on()
-                    {
-                        if let Some(backchannel) = backchannel {
-                            if backchannel.is_open() {
-                                backchannel.send_msg(crate::app_outer::BackchannelMsg::Copy {
-                                    text: replay_link.clone(),
-                                    share: ShareType::Url,
-                                });
-                            } else {
-                                ui.ctx().output_mut(|o| o.copied_text = replay_link.clone());
-                            }
-                        } else {
-                            ui.ctx().output_mut(|o| o.copied_text = replay_link.clone());
-                        }
-
-                        self.replay_copied = true;
-                    }
-
-                    ui.add_space(ui.available_height() * 0.01);
-                }
-
-                let button_text = if self.share_copied {
-                    "COPIED TEXT!"
+            if let Some(replay_link) = &self.replay_link {
+                let button_text = if self.replay_copied {
+                    "COPIED LINK!"
                 } else {
-                    "SHARE BEST SCORE"
+                    "SHARE REPLAY"
                 };
                 let text = TextHelper::heavy(button_text, 12.0, None, ui);
                 let share_button =
@@ -138,119 +105,55 @@ impl DailyActions {
                     if let Some(backchannel) = backchannel {
                         if backchannel.is_open() {
                             backchannel.send_msg(crate::app_outer::BackchannelMsg::Copy {
-                                text: self.msg_mock.share_text.clone(),
-                                share: ShareType::Text,
+                                text: replay_link.clone(),
+                                share: ShareType::Url,
                             });
                         } else {
-                            ui.ctx()
-                                .output_mut(|o| o.copied_text = self.msg_mock.share_text.clone());
+                            ui.ctx().output_mut(|o| o.copied_text = replay_link.clone());
                         }
+                    } else {
+                        ui.ctx().output_mut(|o| o.copied_text = replay_link.clone());
+                    }
+
+                    self.replay_copied = true;
+                }
+
+                ui.add_space(ui.available_height() * 0.01);
+            }
+
+            let button_text = if self.share_copied {
+                "COPIED TEXT!"
+            } else {
+                "SHARE BEST SCORE"
+            };
+            let text = TextHelper::heavy(button_text, 12.0, None, ui);
+            let share_button =
+                text.centered_button(theme.button_primary, theme.text, map_texture, ui);
+
+            if share_button.clicked()
+                || share_button.drag_started()
+                || share_button.is_pointer_button_down_on()
+            {
+                if let Some(backchannel) = backchannel {
+                    if backchannel.is_open() {
+                        backchannel.send_msg(crate::app_outer::BackchannelMsg::Copy {
+                            text: self.msg_mock.share_text.clone(),
+                            share: ShareType::Text,
+                        });
                     } else {
                         ui.ctx()
                             .output_mut(|o| o.copied_text = self.msg_mock.share_text.clone());
                     }
-
-                    self.share_copied = true;
+                } else {
+                    ui.ctx()
+                        .output_mut(|o| o.copied_text = self.msg_mock.share_text.clone());
                 }
 
-                self.msg_mock.render(ui, theme, map_texture);
+                self.share_copied = true;
             }
+
+            self.msg_mock.render(ui, theme, map_texture);
         });
-
-        // let msg = if self.share_copied {
-        //     "COPIED TEXT!"
-        // } else {
-        //     "COPY SUMMARY"
-        // };
-        // let text = TextHelper::heavy(msg, 12.0, None, ui);
-        // let share_button = text.centered_button(theme.button_primary, theme.text, map_texture, ui);
-        // // Extra events to get this message through the backchannel early,
-        // // as our frontend relies on attaching the copy to a browser event
-        // // on mouseup/touchend.
-        // if share_button.clicked()
-        //     || share_button.drag_started()
-        //     || share_button.is_pointer_button_down_on()
-        // {
-        //     if let Some(backchannel) = backchannel {
-        //         if backchannel.is_open() {
-        //             backchannel.send_msg(crate::app_outer::BackchannelMsg::Copy {
-        //                 text: self.share_text.clone(),
-        //             });
-        //         } else {
-        //             ui.ctx()
-        //                 .output_mut(|o| o.copied_text = self.share_text.clone());
-        //         }
-        //     } else {
-        //         ui.ctx()
-        //             .output_mut(|o| o.copied_text = self.share_text.clone());
-        //     }
-
-        //     self.share_copied = true;
-        // }
-
-        // if let Some(replay_link) = &self.replay_link {
-        //     let msg = if self.replay_copied {
-        //         "COPIED TEXT!"
-        //     } else {
-        //         "COPY LINK TO REPLAY"
-        //     };
-        //     let text = TextHelper::heavy(msg, 12.0, None, ui);
-        //     let replay_button =
-        //         text.centered_button(theme.button_secondary, theme.text, map_texture, ui);
-        //     // Extra events to get this message through the backchannel early,
-        //     // as our frontend relies on attaching the copy to a browser event
-        //     // on mouseup/touchend.
-        //     if replay_button.clicked()
-        //         || replay_button.drag_started()
-        //         || replay_button.is_pointer_button_down_on()
-        //     {
-        //         if let Some(backchannel) = backchannel {
-        //             if backchannel.is_open() {
-        //                 backchannel.send_msg(crate::app_outer::BackchannelMsg::Copy {
-        //                     text: replay_link.clone(),
-        //                 });
-        //             } else {
-        //                 ui.ctx().output_mut(|o| o.copied_text = replay_link.clone());
-        //             }
-        //         } else {
-        //             ui.ctx().output_mut(|o| o.copied_text = replay_link.clone());
-        //         }
-
-        //         self.replay_copied = true;
-        //     }
-        // }
-
-        // if self.won_today {
-        //     self.msg_mock.render(ui, theme, map_texture, backchannel);
-        // }
-        // if won_today {
-        //     daily.msg_mock.render(ui, theme, map_texture, backchannel);
-        // } else {
-        //     ui.add_space(20.0);
-
-        //     let mut textrow = |string: String| {
-        //         let row = TextHelper::heavy(&string, 14.0, Some(ui.available_width()), &mut ui);
-        //         row.paint(Color32::WHITE, ui, true);
-        //     };
-
-        //     if won_yesterday {
-        //         textrow("Try again".into());
-        //         textrow("to maintain".into());
-        //         textrow("your streak!".into());
-        //     } else {
-        //         textrow("No worries,".into());
-        //         textrow("have another go!".into());
-        //     }
-
-        //     ui.add_space(16.0);
-
-        //     let text = TextHelper::heavy("TRY AGAIN", 12.0, None, ui);
-        //     let try_again_button =
-        //         text.centered_button(theme.button_primary, theme.text, map_texture, ui);
-        //     if try_again_button.clicked() {
-        //         msg = Some(ResultModalAction::TryAgain);
-        //     }
-        // }
 
         msg
     }
