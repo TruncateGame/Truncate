7dd86cd5eea23b59bb07b723e481da6eadf5559c
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2024-09-08 12:31:21 +1200

    Use consistent hand ordering from the core game

--- truncate_client/src/regions/active_game/mod.rs
+++ truncate_client/src/regions/active_game/mod.rs
@@ -339,7 +339,7 @@ impl ActiveGame {
             player_number: this_player_number,
             next_player_number,
             board,
-            hand: _,
+            hand,
             changes,
             game_ends_at,
             paused,
@@ -385,27 +385,7 @@ impl ActiveGame {
                 .insert(board_change.detail.coordinate, board_change.clone());
         }
 
-        for hand_change in changes.iter().filter_map(|c| match c {
-            Change::Hand(change) => Some(change),
-            _ => None,
-        }) {
-            let player_index = self
-                .depot
-                .gameplay
-                .player_numbers
-                .iter()
-                .position(|p| *p == hand_change.player as u64);
-
-            if let Some(player_index) = player_index {
-                for removed in &hand_change.removed {
-                    if let Some(pos) = self.hands[player_index].iter().position(|t| t == removed) {
-                        self.hands[player_index].remove(pos);
-                    }
-                }
-                let reduced_length = self.hands[player_index].len();
-                self.hands[player_index].0.extend(&hand_change.added);
-            }
-        }
+        self.hands[this_player_number as usize] = hand;
 
         self.time_changes = changes
             .iter()
